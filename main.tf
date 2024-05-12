180163e8803c853f10506652d8fea0d4e81784e0
Simon Lynch <simon.lynch@hashicorp.com>
2024-05-13 08:55:09 +1000

    update

--- main.tf
+++ main.tf
@@ -1,23 +1,30 @@
 
 data "tfe_outputs" "domain" {
   organization = var.tfc_organization
-  workspace    = var.datazone_workspace_name
+  workspace    = local.datazone_env.tfe_workspace
 }
 
 locals {
   datazone_config          = yamldecode(file("${path.module}/config/${var.datazone_domain_yaml_file}"))
   datazone_domain   = local.datazone_config.datazone
-  datazone_env      = local.datazone_config.datazone_environment
+  datazone_env      = local.datazone_config.datazone_environments
+  domain_existing   = coalesce(local.datazone_config.domain_existing, {})
+}
+
+output "tfe_outputs" {
+  value = data.tfe_outputs.domain.nonsensitive_values
 }
 
 module "datazone_environment" {
   source  = "app.terraform.io/tfc-demo-au/datazone-environments/awscc"
   version = "~>  0.1.5"
 
-  region                        = var.region
+  for_each = local.domain_existing
+
+  region                        = local.datazone_env.region
   domain_id                     = coalesce(var.domain_id, data.tfe_outputs.domain.values.datazone_domain_id)
   project_map                   = coalesce(var.project_map, data.tfe_outputs.domain.values.projects)
   blueprint_map                 = coalesce(var.blueprint_map, data.tfe_outputs.domain.values.datazone_environment_blueprints)
-  datazone_environment_profiles = var.datazone_environment_profiles
-  datazone_environments         = var.datazone_environments
+  datazone_environment_profiles = local.datazone_env.datazone_environment_profiles
+  datazone_environments         = local.datazone_env.datazone_environments
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,29 @@
 variable "datazone_domain_yaml_file" {
   description = "file name for datazone yaml"
   type        = string
+  default = "rbs_payments.yaml"
+}
+
+variable "tfc_organization" {
+  description = "The Terraform Cloud organization"
+  type        = string
+  default = "tfc-demo-au"
+}
+
+variable "domain_id" {
+  description = "The domain to deploy the environment"
+  type        = string
+  default     = null
+}
+
+variable "project_map" {
+  description = "The project to deploy the environment"
+  type        = any
+  default     = null
+}
+
+variable "blueprint_map" {
+  description = "The environment blueprint to deploy"
+  type        = any
+  default     = null
 }
